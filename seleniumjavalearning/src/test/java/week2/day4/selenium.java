0864b993a818148003e2765beb0638ddd396f4e7
RJ <RJ@DESKTOP-7IV4I9T>
2024-01-17 13:31:40 +0530

    commit

--- seleniumjavalearning/src/test/java/week2/day4/selenium.java
+++ seleniumjavalearning/src/test/java/week2/day4/selenium.java
@@ -8,18 +8,31 @@ public class selenium {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Instantiate the browserdriver 
-				//shortcut -->ctrl shift o -->import the packages of the classes
+		//System.setProperty("webdriver.chromedriver.driver", "C:\\Users\\RJ\\Documents\\Drv\\chromedriver.exe");
+				
+		//shortcut -->ctrl shift o -->import the packages of the classes
 				ChromeDriver driver =new ChromeDriver();
+				
 				//load the application in the browser
 				driver.get("http://leaftaps.com/opentaps/control/main");
+				
 				//maximize the application
 				driver.manage().window().maximize();
+				
 				//identifying the elements
 				driver.findElement(By.id("username")).sendKeys("DemoCSR");
+				
 				//NoSuchelement
 				driver.findElement(By.id("password")).sendKeys("crmsfa");
 				driver.findElement(By.className("decorativeSubmit")).click();
-
+				driver.findElement(By.linkText("CRM/SFA")).click();
+				driver.findElement(By.linkText("Create Lead")).click();
+				driver.findElement(By.className("inputBox")).sendKeys("abcd");
+				driver.findElement(By.id("createLeadForm_firstName")).sendKeys("abcd");
+				driver.findElement(By.id("createLeadForm_lastName")).sendKeys("abcd");
+				driver.findElement(By.className("smallSubmit")).click();
+				
+				driver.quit();
 		
 	}
 
