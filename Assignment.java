8e358855c95738961d0ab6aeadca318b2b268d38
Amrutakulkarni123 <156309950+Amrutakulkarni123@users.noreply.github.com>
2024-01-19 16:38:15 +0530

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -3,7 +3,7 @@ package week2.day4;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Assignment {
+public class Assignment_diffLocators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
